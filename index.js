55c5d069c84f207ddf796f17c47e1231c39178ba
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2020-04-22 23:42:10 -0400

    sets the variable to html elements

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 let alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' ];
 
 let words = [ 'APPLE', "HELLO", "OCEAN", 'SUMMER'];
+
+let hangman = document.querySelector('.hangman');
+let letterDiv = document.querySelector('.letters');
+let wordDiv = document.querySelector('.word-div');
+let underlineDiv = document.querySelector('.underline-div');
+let letters = document.getElementsByClassName('letter');
+let wordSpan = document.getElementsByClassName('word-span');
